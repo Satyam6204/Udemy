5f9cc2d3ddfd8486293ad3ec3899a4cc6b58d6b0
Satyam6204 <satu45025@gmai.com>
2024-12-23 00:37:22 +0530

    second Update

---
+++ index.js
@@ -0,0 +1,16 @@
+let i1 = document.getElementById('img12')
+
+function imgf1() {
+    i1.src = 'https://cms-images.udemycdn.com/96883mtakkm8/4kbyXne3Slx9Sfz4nTBqdf/8ac2b75db1a118f15e2fb5dfe2bb4567/desktop-hands-on-learning-2x.png';
+}
+function imgf2() {
+    i1.src = 'https://cms-images.udemycdn.com/96883mtakkm8/GUVYFTj0uwEQuJha5j7TZ/133c991fb3b3f1f93a3e842f4baa7f44/desktop-certification-prep-2x.png';
+}
+
+function imgf3() {
+    i1.src = 'https://cms-images.udemycdn.com/96883mtakkm8/6q4N9BvIQusFoheoALJhGj/678c1a0bb6c2a22d95461d409492231e/desktop-insights-and-analytics-2x.png';
+}
+
+function imgf4() {
+    i1.src = 'https://cms-images.udemycdn.com/96883mtakkm8/385IhnON960Wvz50ooWIN3/d4e6738c97769258d387b3d609edaad4/desktop-customizable-2x.png';
+}
